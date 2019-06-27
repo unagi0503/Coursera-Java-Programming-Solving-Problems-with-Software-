c6a0fd25b376f7ac7f847e4218f72160c4b005b5
SenNoKizu <bud1027@gmail.com>
2019-06-27 18:21:34 +0900

    added countryInfo method, assignment 1-2

--- week03-FirstCSVExample/Export data csv files, assingment codes/WhichCountriesExport.java
+++ week03-FirstCSVExample/Export data csv files, assingment codes/WhichCountriesExport.java
@@ -33,7 +33,7 @@ public class WhichCountriesExport {
 	
 	
 	/* 
-	Assignment 01
+	Assignment week 3: 1-1
 	
 	Write a method named tester that will create your CSVParser and call each of the methods below in parts 2, 3, 4, and 5. You would start your code with:
 	
@@ -48,7 +48,46 @@ public class WhichCountriesExport {
 	public void tester (){
 		FileResource fr = new FileResource();
 		CSVParser parser = fr.getCSVParser();
+		
+		String result = countryInfo(parser, "Japan");
+		System.out.println(result);
 	}  
 	
 	
+	
+	
+	/*
+	Assignment week 3: 1-2
+	
+	Write a method named countryInfo that has two parameters, parser is a CSVParser and country is a String. This method returns a string of information about the country or returns “NOT FOUND” if there is no information about the country. The format of the string returned is the country, followed by “: “, followed by a list of the countries’ exports, followed by “: “, followed by the countries export value. For example, using the file exports_small.csv and the country Germany, the program returns the string:
+	
+		Germany: motor vehicles, machinery, chemicals: $1,547,000,000,000
+	
+	*/
+	
+	public String countryInfo (CSVParser parser, String country) {
+		
+		String info = "NOT FOUND";
+		String exports;
+		String value;
+		
+		for (CSVRecord record : parser) {
+		
+			String countryName = record.get("Country");
+			
+			if (countryName.equals(country)) {
+				
+				exports = record.get("Exports");
+				value = record.get("Value (dollars)");
+				
+				info = countryName + ": " + exports + ": " + value + ": ";
+				
+				
+				return info;
+			} 			
+		}
+		
+		return info;
+		
+	}
 }
