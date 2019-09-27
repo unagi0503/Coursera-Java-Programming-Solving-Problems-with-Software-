756c37314f811d3ad12253d54b9c64182c7c63f9
subutti0503 <subutti0503@gmail.com>
2019-09-27 20:44:24 +0900

    week-04-babyname in progress

--- week04-Baby-Names-Assignments/BabyBirths.java
+++ week04-Baby-Names-Assignments/BabyBirths.java
@@ -23,32 +23,52 @@ public class BabyBirths {
 		int totalBirths = 0;
 		int totalBoys = 0;
 		int totalGirls = 0;
-		int totalGirlsName = 0;
-		int totalBoysName = 0;
+		int totalGirlsNames = 0;
+		int totalBoysNames = 0;
 		
 		for (CSVRecord rec : fr.getCSVParser(false)) {
 			int numBorn = Integer.parseInt(rec.get(2));
 			totalBirths += numBorn;
 			if (rec.get(1).equals("M")) {
 				totalBoys += numBorn;
-				totalBoysName += 1;
+				totalBoysNames += 1;
 			}
 			else {
 				totalGirls += numBorn;
-				totalGirlsName += 1;
+				totalGirlsNames += 1;
 			}
 		}
 		System.out.println("total births = " + totalBirths);
 		System.out.println("female girls = " + totalGirls);
 		System.out.println("male boys = " + totalBoys);
+		System.out.println("female girls names = " + totalGirlsNames);
+		System.out.println("male boys names = " + totalBoysNames);
 	}
 
 	public void testTotalBirths () {
 		//FileResource fr = new FileResource();
-		FileResource fr = new FileResource("data/yob2014.csv");
+		FileResource fr = new FileResource();
 		totalBirths(fr);
 	}
+	
+	public int getRank (int year, String name, String gender) {
+		
+		int Rank = 0;
+		String dataName = "yob" + year;
+		FileResource fr = new FileResource("us_babynames/us_babynames_by_year/" + dataName);
+		
+		for (CSVRecord rec : fr.getCSVParser(false)) {
+			if (rec.get(1).equals(gender)) {
+				if (rec.get(0).equals(name)) {
+					
+				}
+			}
+		}
+		
+		return 0;
+	}
 }
+
 
 
 
